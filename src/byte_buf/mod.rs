5eb6dbcf1d8d3ebfd0fceb9d4f8f11a1b86b313c
yl <yanlsix@163.com>
2023-01-08 23:19:27 +0800

    first commit

---
+++ src/byte_buf/mod.rs
@@ -0,0 +1,7 @@
+pub use error::{ByteBufResult, ErrorType};
+pub use byte_buf_faced::{ByteBuf};
+pub use read_byte_buf::ReadByteBuf;
+
+pub mod error;
+pub mod byte_buf_faced;
+pub mod read_byte_buf;
